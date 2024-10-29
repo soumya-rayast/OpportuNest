5c319bc57f59a2d005e7ad059a9d8524f58525af
Soumya Rayast <113257526+soumya-rayast@users.noreply.github.com>
2024-10-29 21:09:29 +0530

    minor update

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: process.env.CLIENT_URL,
     credentials: true,
 };
 app.use(cors(corsOptions));
