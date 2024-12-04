762af54ad0d2b1afd90f4bd9e1cd2c124d2eee36
Soumya Rayast <113257526+soumya-rayast@users.noreply.github.com>
2024-12-04 16:10:47 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http://localhost:5173/',
+    origin: 'https://opportunest-1-s7oc.onrender.com',
     credentials: true, 
 };
 app.use(cors(corsOptions));
